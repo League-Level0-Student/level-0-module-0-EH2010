c07d0d74d93e707c926a8866c5c547fba7440e1a
EH2010 <53235430+EH2010@users.noreply.github.com>
2019-07-24 23:44:06 +0300

    Create Osaaan.java

---
+++ src/Osaaan.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+public class Osaaan {
+
+	
+	public static void main(String[] args) {
+				
+		
+	Random ran = new Random();
+	
+	int osaaan = ran.nextInt(12);
+	
+	System.out.println(osaaan);
+	
+	
+
+	}
+	
+
+}
