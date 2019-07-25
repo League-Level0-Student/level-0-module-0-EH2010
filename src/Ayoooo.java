4a65c308a73eda422e6868655cd88ce06f972131
EH2010 <53235430+EH2010@users.noreply.github.com>
2019-07-25 16:54:16 +0300

    2507

---
+++ src/Ayoooo.java
@@ -0,0 +1,21 @@
+
+public class Ayoooo { 
+	
+	public static void main(String[] args) {
+		
+		String isim = "Hello" + " Mello";
+		String soyisim = " Cello ";
+		System.out.println(isim+soyisim+isim);
+		 
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}}
