d6614b185e5cd08e6139199fda26cbce45a5d174
EH2010 <53235430+EH2010@users.noreply.github.com>
2019-07-27 12:26:23 +0300

    Update Ayoooo.java

--- src/Ayoooo.java
+++ src/Ayoooo.java
@@ -3,19 +3,36 @@ public class Ayoooo {
 	
 	public static void main(String[] args) {
 		
-		String isim = "Hello" + " Mello";
-		String soyisim = " Cello ";
-		System.out.println(isim+soyisim+isim);
-		 
+		//String isim = "Hello" + " Mello";
+		//String soyisim = " Cello ";
+		//String ad = " tello";
 		
+		//System.out.println("merhaba "+ isim+soyisim+isim+ad + ad);
+		 
+		int a=  1;
+     int b =  2;
+     // int c = a+b ;
+	// System.out.println((a+b) * 3);
+	 
+	if (a!=b) {
+		System.out.println("Aynen " +3);
+	
+	
+	}
+	
+	
+	
+	else {System.out.println("Taaam");
+		
+	}
+	
+	System.out.println("Ayoooo");
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	}}
+	}
+		
+
+}
